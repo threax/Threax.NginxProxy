f6deb901cd63ca484abe56e66878a557ab95be42
Andrew Piper <piper.andrew@spcollege.edu>
2019-01-29 13:23:42 -0500

    Added a swarm mode.

--- NginxProxy/Program.cs
+++ NginxProxy/Program.cs
@@ -58,7 +58,7 @@ namespace DockerClient
         {
             var config = new DockerClientConfiguration(new Uri(host));
             var client = config.CreateClient();
-            IEnumerable<ContainerListResponse> containers;
+            IEnumerable<ContainerNetworkInfo> containers;
             if (swarmMode)
             {
                 containers = await GetSwarmContainers(network, client);
@@ -67,20 +67,9 @@ namespace DockerClient
             {
                 containers = await GetContainers(network, client);
             }
-            var networkContainers = containers.Where(i => i.NetworkSettings.Networks.ContainsKey(network));
-            var threaxNginxLabeled = networkContainers.Where(i => i.GetThreaxHost() != null)
-                .Select(i => new ContainerNetworkInfo
-                {
-                    Host = i.GetThreaxHost(),
-                    Port = i.GetThreaxPort(),
-                    Name = i.Names.FirstOrDefault(),
-                    Image = i.Image,
-                    Ip = i.NetworkSettings.Networks[network].IPAddress,
-                    MaxBodySize = "0" //i.GetThreaxMaxBodySize() //For now have to use 0 here, doesnt work otherwise
-                });
 
             var configWriter = new NginxConfWriter();
-            var nginxConfig = configWriter.GetConfig(threaxNginxLabeled);
+            var nginxConfig = configWriter.GetConfig(containers);
 
             bool updateFile;
             using (var reader = new StreamReader(File.Open(outFile, FileMode.OpenOrCreate)))
@@ -92,9 +81,9 @@ namespace DockerClient
             if (updateFile)
             {
                 Console.WriteLine("New mappings found.");
-                foreach (var item in threaxNginxLabeled)
+                foreach (var item in containers)
                 {
-                    Console.WriteLine($"Mapping {item.Host} to {item.Ip} for container {item.Name} from {item.Image}");
+                    Console.WriteLine($"Mapping {item.Host} with port {item.Port} to {item.Ip} for container {item.Name} from {item.Image}");
                 }
 
                 using (var writer = new StreamWriter(File.Open(outFile, FileMode.Create)))
@@ -106,23 +95,46 @@ namespace DockerClient
             return updateFile;
         }
 
-        private static async Task<IEnumerable<ContainerListResponse>> GetContainers(string network, Docker.DotNet.DockerClient client)
+        private static async Task<IEnumerable<ContainerNetworkInfo>> GetContainers(string network, Docker.DotNet.DockerClient client)
         {
-            return await client.Containers.ListContainersAsync(new ContainersListParameters());
+            var containers = await client.Containers.ListContainersAsync(new ContainersListParameters());
+            var networkContainers = containers.Where(i => i.NetworkSettings.Networks.ContainsKey(network));
+            return networkContainers.Where(i => i.GetThreaxHost() != null)
+                .Select(i => new ContainerNetworkInfo
+                {
+                    Host = i.GetThreaxHost(),
+                    Port = i.GetThreaxPort(),
+                    Name = i.Names.FirstOrDefault(),
+                    Image = i.Image,
+                    Ip = i.NetworkSettings.Networks[network].IPAddress,
+                    MaxBodySize = "0" //i.GetThreaxMaxBodySize() //For now have to use 0 here, doesnt work otherwise
+                });
         }
 
-        private static async Task<IEnumerable<ContainerListResponse>> GetSwarmContainers(string network, Docker.DotNet.DockerClient client)
+        private static async Task<IEnumerable<ContainerNetworkInfo>> GetSwarmContainers(string network, Docker.DotNet.DockerClient client)
         {
-            var nodes = await client.Swarm.ListNodesAsync();
-            var containers = new List<ContainerListResponse>();
-            foreach(var node in nodes.Where(i => i.ManagerStatus.Reachability == "reachable"))
-            {
-                Console.WriteLine($"Found node: {node.Description.Hostname} at {node.ManagerStatus.Addr}");
-                var config = new DockerClientConfiguration(new Uri(node.ManagerStatus.Addr));
-                var nodeClient = config.CreateClient();
-                containers.AddRange(await nodeClient.Containers.ListContainersAsync(new ContainersListParameters()));
-            }
-            return containers;
+            var networkInfo = await client.Networks.InspectNetworkAsync(network);
+            var services = await client.Swarm.ListServicesAsync();
+            var networkServices = services.Where(i => i.Endpoint.VirtualIPs.Any(ip => ip.NetworkID == networkInfo.ID));
+            return networkServices.Where(i => i.GetThreaxHost() != null)
+                .Select(i =>
+                {
+                    var ip = i.Endpoint.VirtualIPs.Where(n => n.NetworkID == networkInfo.ID).Select(j => j.Addr).First();
+                    var slashIndex = ip.IndexOf('/');
+                    if(slashIndex != -1)
+                    {
+                        ip = ip.Substring(0, slashIndex);
+                    }
+                    return new ContainerNetworkInfo
+                    {
+                        Host = i.GetThreaxHost(),
+                        Port = i.GetThreaxPort(),
+                        Name = i.Spec.Name,
+                        Image = i.Spec.TaskTemplate.ContainerSpec.Image,
+                        Ip = ip,
+                        MaxBodySize = "0" //i.GetThreaxMaxBodySize() //For now have to use 0 here, doesnt work otherwise
+                    };
+                });
         }
     }
 }
---
+++ NginxProxy/SwarmServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Docker.DotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DockerClient
+{
+    public static class SwarmServiceExtensions
+    {
+        public static String GetThreaxHost(this SwarmService response, bool throwOnMissing = false)
+        {
+            return ReadLabel(response, throwOnMissing, "threax.nginx.host");
+        }
+
+        public static String GetThreaxPort(this SwarmService response, bool throwOnMissing = false)
+        {
+            return ReadLabel(response, throwOnMissing, "threax.nginx.port");
+        }
+
+        public static String GetThreaxMaxBodySize(this SwarmService response, bool throwOnMissing = false)
+        {
+            return ReadLabel(response, throwOnMissing, "threax.nginx.maxbodysize");
+        }
+
+        private static string ReadLabel(SwarmService response, bool throwOnMissing, string label)
+        {
+            if (response.Spec.TaskTemplate.ContainerSpec.Labels.TryGetValue(label, out var result))
+            {
+                return result;
+            }
+            else if (throwOnMissing)
+            {
+                throw new Exception($"Cannot find label {label}.");
+            }
+
+            return null;
+        }
+    }
+}
